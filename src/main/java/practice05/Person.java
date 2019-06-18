7d8ea7cd307909a50043c3a5b1c60c120d6b18fc
Zhou Li <tiramisu101111@gmail.com>
2019-06-18 18:02:52 +0800

    finish practice05 (same as practice 04)

--- src/main/java/practice05/Person.java
+++ src/main/java/practice05/Person.java
@@ -1,4 +1,24 @@
 package practice05;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public Person(String myName, int myAge) {
+        name = myName;
+        age = myAge;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %s years old.", name, age);
+    }
 }
+// practice05 is same as practice04
