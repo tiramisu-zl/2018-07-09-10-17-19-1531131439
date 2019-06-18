632a4a66e4daee304f122cf8ff126150de15a4da
Zhou Li <tiramisu101111@gmail.com>
2019-06-18 17:54:37 +0800

    finish practice03

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,13 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person{
+
+    public Worker(String myName, int myAge) {
+        super(myName, myAge);
+    }
+
+
+    public String introduce() {
+        return String.format("I am a Worker. I have a job.");
+    }
 }
