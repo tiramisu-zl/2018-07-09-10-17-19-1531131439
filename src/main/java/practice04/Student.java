daeef0e9feb94f38488d44d9246486dce83d3b30
Zhou Li <tiramisu101111@gmail.com>
2019-06-18 18:00:00 +0800

    finish practice04

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,16 @@
 package practice04;
 
-public class Student {
+public class Student extends Person{
+    private Number klass;
+
+    public Student(String myName, int myAge, Number myKlass) {
+        super(myName, myAge);
+        klass = myKlass;
+    }
+
+    public Number getKlass() {return klass;}
+
+    public String introduce() {
+        return String.format("My name is %s. I am %s years old. I am a Student. I am at Class %s.", getName(), getAge(), klass);
+    }
 }
--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,12 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker(String myName, int myAge) {
+        super(myName, myAge);
+    }
+
+
+    public String introduce() {
+        return String.format("My name is %s. I am %s years old. I am a Worker. I have a job.", getName(), getAge());
+    }
 }
