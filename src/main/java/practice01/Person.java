68607556675dfce3dec2be69b150feb9f3e0559c
Zhou Li <tiramisu101111@gmail.com>
2019-06-18 02:07:53 -0700

    finish practice01

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,19 @@
 package practice01;
 
 public class Person {
+    private String name;
+    private Number age;
+
+    public Person(String myName, Number myAge) {
+        name = myName;
+        age = myAge;
+    }
+
+    public String getName() { return name; }
+    public Number getAge() { return age; }
+
+    public String introduce() {
+//        return "My name is " + name + ". I am " + age + " years old.";
+        return String.format("My name is %s. I am %s years old.", name, age);
+    }
 }
