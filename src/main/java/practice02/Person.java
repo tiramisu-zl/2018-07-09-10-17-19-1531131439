17b9ece380f5b30a399e272a3ed4a08293918fb1
Zhou Li <tiramisu101111@gmail.com>
2019-06-18 17:48:03 +0800

    finish practice02

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,51 @@
 package practice02;
 
+import java.util.Objects;
+
 public class Person {
+    private String name;
+    private int age;
+
+    public Person(String myName, int myAge) {
+        name = myName;
+        age = myAge;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return getAge() == person.getAge() &&
+                Objects.equals(getName(), person.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getAge());
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %s years old.", name, age);
+    }
 }
+
+// 构造器，如果没有定义构造器，会有默认的，如果只有自定义构造器就不能直接调用默认构造器
+// 自动生成construtor, getter, setter, toString, equals
+// 对象类型允许为null, 基础类型不允许
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,17 @@
 package practice02;
 
-public class Student {
+public class Student extends Person {
+    private Number klass;
+
+    public Student(String myName, int myAge, Number myKlass) {
+        super(myName, myAge);
+        klass = myKlass;
+    }
+
+    public Number getKlass() {return klass;}
+
+    public String introduce() {
+        return String.format("I am a Student. I am at Class %s.", klass);
+    }
+
 }
