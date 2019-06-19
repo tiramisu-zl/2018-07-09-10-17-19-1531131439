edd1146aa4d76ba3d4e2b308d3dd28f95053bdd5
Li Zhou <tiramisu101111@gmail.com>
2019-06-19 10:17:56 +0800

    finish practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,43 @@
 package practice09;
 
+import java.lang.reflect.Array;
+
 public class Klass {
+    private Number number;
+    private Student leader;
+
+    public Klass(Number number) {
+        this.number = number;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public Number getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %s", number);
+    }
+
+    public void appendMember(Student student) {
+        // 该方法意味将一名学生加入该班级。但是没有明确怎么加入, 需要新加一个变量存append student？
+        // 在new Student 时传的klass number 不是就可以确认班级了吗？
+        // 如果student 有klass number, 但是klass 没有append，那这个学生算不算这个班级的呢？
+        System.out.println("append success");
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass().getNumber() == number) { // 如果有一个studentList属性来存append的student,则判断studentList里面是否包含该学生
+            this.setLeader(student);
+        } else {
+            System.out.println("It is not one of us.");
+        }
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,34 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int myId, String myName, int myAge) {
+        super(myId, myName, myAge);
+    }
+
+    public Teacher(int myId, String myName, int myAge, Klass myKlass) {
+        super(myId, myName, myAge);
+        klass = myKlass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if (this.getKlass() != null) {
+            return String.format("My name is %s. I am %s years old. I am a Teacher. I teach Class %s.", getName(), getAge(), klass.getNumber());
+        } else {
+            return String.format("My name is %s. I am %s years old. I am a Teacher. I teach No Class.", getName(), getAge());
+        }
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().getNumber() == klass.getNumber()) {
+            return String.format("My name is %s. I am %s years old. I am a Teacher. I teach %s.", getName(), getAge(), student.getName());
+        } else {
+            return String.format("My name is %s. I am %s years old. I am a Teacher. I don't teach %s.", getName(), getAge(), student.getName());
+        }
+    }
 }
